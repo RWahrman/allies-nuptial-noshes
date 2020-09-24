741008a18a1edaad7cafcbd78e58abe87586ff6f
RWahrman <rebecca.wahrman@gmail.com>
2020-09-24 13:10:00 -0400

    got Provider in the right place

---
+++ client/store/index.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import reducer from "./reducer";
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+
+export default store;
