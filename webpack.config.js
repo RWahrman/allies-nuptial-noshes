01146c03a43db62f9e6ff111facf06ca95abe404
RWahrman <rebecca.wahrman@gmail.com>
2020-09-24 11:52:30 -0400

    added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: "./client/index.js", // assumes your entry point is the index.js in the root of your project folder
+  mode: "development",
+  output: {
+    path: __dirname,
+    filename: "public/bundle.js", //creates a bundle.js file in the public directory
+  },
+  devtool: "source-maps",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
