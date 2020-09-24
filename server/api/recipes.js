748fcd685e15d4e12b16c2468893601ba689b773
RWahrman <rebecca.wahrman@gmail.com>
2020-09-24 11:27:29 -0400

    edited recipe routes

--- server/api/recipes.js
+++ server/api/recipes.js
@@ -10,11 +10,11 @@ router.post("/", function (req, res, next) {
   /* etc */
 });
 // matches PUT requests to /api/puppies/:puppyId
-router.put("/:puppyId", function (req, res, next) {
+router.put("/:recipe-slug", function (req, res, next) {
   /* etc */
 });
 // matches DELETE requests to /api/puppies/:puppyId
-router.delete("/:puppyId", function (req, res, next) {
+router.delete("/:recipe-slug", function (req, res, next) {
   /* etc */
 });
 
