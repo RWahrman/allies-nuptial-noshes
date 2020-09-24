0d807a20bc847bdb1eb4633979635283cd893487
RWahrman <rebecca.wahrman@gmail.com>
2020-09-24 10:51:49 -0400

    add express static routes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 // Entry point for SERVER JavaScript
 
+const path = require("path");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// Any routes or other various middlewares should go here!
+
+// Make sure this is right at the end of your server logic!
+// The only thing after this might be a piece of middleware to serve up 500 errors for server problems
+// (However, if you have middleware to serve up 404s, that go would before this as well)
+
+app.get("*", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+});
