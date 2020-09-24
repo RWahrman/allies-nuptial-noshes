68f31ff2a0ad6222218c2d236ce64297ef1951ef
RWahrman <rebecca.wahrman@gmail.com>
2020-09-24 12:27:01 -0400

    fixed start command

--- client/index.js
+++ client/index.js
@@ -1,1 +1,8 @@
 // Entry point for CLIENT JavaScript
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>Hello, world!</div>,
+  document.getElementById("app") // make sure this is the same as the id of the div in your index.html
+);
