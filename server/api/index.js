ca43b1ba56a5bd97430d151a01f2b14debbc749a
RWahrman <rebecca.wahrman@gmail.com>
2020-09-24 11:27:57 -0400

    stick to recipe routes

--- server/api/index.js
+++ server/api/index.js
@@ -3,10 +3,10 @@ const router = require("express").Router();
 //main route for recipes
 router.use("/recipes", require("./recipes"));
 
-//for security??
-router.use("/users", require("./users"));
+// //for security??
+// router.use("/users", require("./users"));
 
-// bios maybe
-router.use("/biographies", require("./biogrphies"));
+// // bios maybe
+// router.use("/biographies", require("./biogrphies"));
 
 module.exports = router;
